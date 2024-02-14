50b96d733c960ee20e6e72d9211ec70bb03d3f01
Ekaterina <90846568+EkaterinaSimachkova@users.noreply.github.com>
2024-02-14 14:48:02 +0300

    Add files via upload

--- tasks/task5.js
+++ tasks/task5.js
@@ -7,3 +7,22 @@ arr.forEach((el) => {
     } else set.add(el)
 })
 console.log([...set][0])
+
+
+// если в массиве числа повторяются больше двух раз, то
+
+let arr2 = [1, 1, 8, 4, 4, 6, 1, 8]
+
+let map = new Map()
+arr2.forEach((el) => {
+    if (map.has(el)){
+        map.set(el, map.get(el)+1)
+    } else map.set(el, 1)
+})
+for (let [el, count] of map.entries()) {
+    if (count == 1) {
+        console.log(el)
+        break
+    }
+}
+
