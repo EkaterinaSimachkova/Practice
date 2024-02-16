ae2c6cbbaf1b6cb04c6dd3b7f75cb0c41122b022
Ekaterina <90846568+EkaterinaSimachkova@users.noreply.github.com>
2024-02-16 18:34:01 +0300

    Add files via upload

--- API/index.js
+++ API/index.js
@@ -8,18 +8,15 @@ import getDogs from './getDogs.js'
 let content = document.getElementById('main-content')
 
 let input = document.getElementById('input')
-const count = input.value
-console.log(count)
 
-let RickAndMorty = document.getElementById('RickAndMorty')
-let Pokemon = document.getElementById('Pokemon')
-let Dog = document.getElementById('Dog')
+let radios = document.querySelectorAll('input[type="radio"]');
+radios.forEach(radio => radio.addEventListener('change', search))
 
-console.log()
+let btnSearch = document.getElementById('button-search')
+btnSearch.addEventListener("click", search)
 
 let btnFlex = document.getElementById('button-flex')
 let btnGrid = document.getElementById('button-grid')
-
 btnFlex.addEventListener("click", displayFlex)
 btnGrid.addEventListener("click", displayGrid)
 
@@ -27,34 +24,52 @@ btnGrid.addEventListener("click", displayGrid)
 function displayFlex() {
     content.className = 'main-content'
 
-    let imgs = content.querySelectorAll('img')
-    for (let img of imgs) {
-        img.style.width = '150px'
-        img.style.height = '110px'
-    }
-
-    let ps = content.querySelectorAll('p')
-    for (let p of ps) {
-        p.style.display = 'block'
+    let divs = content.querySelectorAll('div')
+    for (let div of divs) {
+        div.className = 'image'
     }
 }
 
 function displayGrid() {
-    content.className = 'grid-main-content'
-
-    let imgs = content.querySelectorAll('img')
-    for (let img of imgs) {
-        img.style.width = '100%'
-        img.style.height = '100%'
-    }
-
-    let ps = content.querySelectorAll('p')
-    for (let p of ps) {
-        p.style.display = 'none'
+    content.className = 'row'
+    content.classList.add('row-cols-4')
+    
+    let divs = content.querySelectorAll('div')
+    for (let [idx, div] of divs.entries()) {
+        div.style.height = '300px'
+        switch (idx % 6) {
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+                div.className = 'col-3'
+                break
+            case 4:
+            case 5:
+            
+                div.className = 'col-6'
+                break
+        }
+        
     }
 }
 
-setImages('Dog')
+function search() {
+    let api
+    for (let radio of radios) {
+		if (radio.checked) {
+			api = radio.value
+		}
+	}
+    let count = input.value
+    if (count == '') {
+        setImages(api)
+    } else if (isFinite(count)) {
+        setImages(api, +count)
+    } else {
+        alert('Введено не число!')
+    }
+}
 
 async function setImages(api, count = 12) {
     try {
@@ -70,10 +85,12 @@ async function setImages(api, count = 12) {
                 images = await getDogs(count)
                 break
         }
-
+        console.log(images)
+        console.log(typeof(images))
+        content.innerHTML = ''
         for (let image of images) {
             content.insertAdjacentHTML('beforeend', `
-                <div class="image"> 
+                <div class='image'> 
                     <img class='image-icon' src=${image.url}></img>
                     <p class='image-title'>${image.name.length > 20 ? image.name.slice(0, 20) + "…" : image.name}</p>
                 </div>
@@ -89,14 +106,13 @@ async function setImages(api, count = 12) {
     }
 }
 
-async function getRickAndMortyCharacters(count = 12) {
+async function getRickAndMortyCharacters(count) {
     try{
-        let countArr = [...Array(count+1).keys()]
+        let countArr = [...Array(count+1).keys()].slice(1)
         let result = []
         const response = await axios.get(`https://rickandmortyapi.com/api/character/${countArr}`)
         let data = response.data
-        data.forEach(item => result.push({id: item.id, name: item.name, url: item.url}))
-        console.log(result)
+        data.forEach(item => result.push({id: item.id, name: item.name, url: item.image}))
         return result
     } catch(error) {
         if (error.response) {
@@ -115,13 +131,17 @@ async function getRickAndMortyCharacters(count = 12) {
     };
 }
 
-async function getPokemonCharacters(count = 12) {
+async function getPokemonCharacters(count) {
     try{
         let result = []
         const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/?limit=${count}`)
         let data = response.data.results
-        data.forEach(item => result.push({id: Number(item.url.match(/\/\d+\//g)[0].slice(1, -1)), name: item.name, url: item.url}))
-        console.log(result)
+        data.forEach(async (item) => {
+            let id = item.url.split('/').at(-2)
+            const pokemon = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
+            console.log(pokemon.data.sprites.front_default)
+            result.push({id: +id, name: item.name, url: pokemon.data.sprites.front_default})
+        })
         return result
     } catch(error) {
         if (error.response) {
@@ -140,13 +160,12 @@ async function getPokemonCharacters(count = 12) {
     };
 }
 
-async function getDogs(count = 12) {
+async function getDogs(count) {
     try{
         let result = []
-        const response = await axios.get(`https://dog.ceo/api/breed/hound/images/random/${count}`)
+        const response = await axios.get(`https://dog.ceo/api/breeds/image/random/${count}`)
         let data = response.data.message
-        data.forEach(item => result.push({id: data.indexOf(item)+1, name: item.match(/\/hound-.+\//g)[0].slice(1, -1), url: item}))
-        console.log(result)
+        data.forEach(item => result.push({id: data.indexOf(item)+1, name: item.split('/').at(-2), url: item}))
         return result
     } catch(error) {
         if (error.response) {
